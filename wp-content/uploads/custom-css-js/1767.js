3f2e7209d6c69f43252a9210dcd7335cd15268d4
sirnatty <SirNatty@proton.me>
2024-06-20 12:02:40 +0200

    Delete wp-content directory

--- wp-content/uploads/custom-css-js/1767.js
+++
@@ -1,8 +0,0 @@
-<!-- start Simple Custom CSS and JS -->
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
-    var head = document.querySelector('.moving-head img');
-    head.classList.add('moving-head');
-  });
-</script>
-<!-- end Simple Custom CSS and JS -->
