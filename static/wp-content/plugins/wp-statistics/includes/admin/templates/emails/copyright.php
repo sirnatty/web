70300d33e19aeee55ce4472e73122c9a65b6d066
sirnatty <SirNatty@proton.me>
2024-06-20 15:37:31 +0200

    Add new static content

---
+++ static/wp-content/plugins/wp-statistics/includes/admin/templates/emails/copyright.php
@@ -0,0 +1,6 @@
+<div class="footer" style="box-sizing: border-box; font-family: 'Roboto',Arial,Helvetica,sans-serif; margin: 0; margin-top: 48px; padding: 0; text-align: center; text-decoration: none;">
+    <span style="box-sizing: border-box; color: #56585a; font-family: 'Roboto',Arial,Helvetica,sans-serif; font-size: 14px; font-style: italic; font-weight: 500; line-height: 16.41px; margin: 0; padding: 0; text-decoration: none;">
+        <?php esc_html_e('Powered by:', 'wp-statistics'); ?>
+        <a href="https://wp-statistics.com?utm_source=wp_statistics&utm_medium=display&utm_campaign=email_report" target="_blank" title="<?php esc_html_e('WP Statistics', 'wp-statistics'); ?>" style="box-sizing: border-box; color: #175da4; font-family: 'Roboto',Arial,Helvetica,sans-serif; font-size: 14px; font-style: italic; font-weight: 500; line-height: 16.41px; margin: 0; padding: 0; text-decoration: none;"><?php esc_html_e('WP Statistics', 'wp-statistics'); ?></a>
+    </span>
+</div>
